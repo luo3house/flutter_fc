16cce162f70035440ecde5605f5171d1fd1d4b4d
luoyz <luoyz2349@outlook.com>
2023-09-06 18:17:16 +0800

    chore: example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,36 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fc/flutter_fc.dart';
 
-Widget _Counter(props) {
-  final (counter, setCounter) = useState(0);
-  return ElevatedButton(
-    onPressed: () => setCounter(counter + 1),
-    child: Text("Counter: $counter"),
-  );
-}
-
-final Counter = defineFC(_Counter);
-
-var flag = 0;
-
-// reproduction: reload, reload again, should clean up hooks and perform "useMemo" rebuild
-Widget _ErrTest(props) {
-  flag++;
-  final value = flag > 1 ? useMemo(() => 1, []) : useRef(0).current;
-  return Text("$value");
-}
-
-final ErrTest = defineFC(_ErrTest);
-
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: SafeArea(
-        child: Column(children: [
-          Counter(),
-          ErrTest(),
-        ]),
-      ),
+  runApp(const MaterialApp(
+    home: SafeArea(
+      child: OverlayHierScreen(),
     ),
   ));
+}
+
+class CounterScreen extends FCWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build() {
+    final (counter, setCounter) = useState(0);
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => setCounter(counter + 1),
+          child: Text("Counter: $counter"),
+        ),
+      ),
+    );
+  }
+}
+
+class ErrorTestScreen extends FCWidget {
+  static var flag = 0;
+
+  const ErrorTestScreen({super.key});
+
+  @override
+  Widget build() {
+    flag++;
+    final value = flag > 1 ? useMemo(() => 1, []) : useRef(0).current;
+    return Text("$value");
+  }
+}
+
+/// ```jsx
+/// const Child = () => {
+///   const fn = useContext(ParentContext);
+///   useEffect(() => fn(), []);
+///   return <></>;
+/// };
+/// const Parent = () => {
+///   const [c, setC] = useState(0);
+///   return (
+///     <ParentContext.Provider value={() => setC(c + 1)}>
+///       <Child />
+///       <div>c = {c}</div>
+///     </ParentContext.Provider>
+///   );
+/// };
+/// ```
+class OverlayHierScreen extends FCWidget {
+  static final Child = defineFC((Function()? callParent) {
+    useEffect(() => callParent?.call());
+    return const SizedBox();
+  });
+
+  const OverlayHierScreen({super.key});
+
+  @override
+  Widget build() {
+    final (counter, setCounter) = useState(0);
+
+    return Overlay(
+      initialEntries: [
+        OverlayEntry(
+          builder: (_) => Scaffold(
+            body: Stack(children: [
+              Child(props: () => setCounter(counter + 1)),
+              Text("counter = $counter"),
+            ]),
+          ),
+        ),
+      ],
+    );
+  }
 }
